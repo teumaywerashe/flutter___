4f016ecb22d1e3905d9cfbb74714a59691636ba2
teumaywerashe <teumay21@gmail.com>
2026-04-22 22:10:38 +0300

    refactor: convert MyHomePage to StatefulWidget and manage TextEditingController

--- lib/main.dart
+++ lib/main.dart
@@ -6,29 +6,55 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: MyHomePage());
   }
 }
 
-class MyHomePage extends StatelessWidget {
+class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextEditingController myController = TextEditingController();
+
+  @override
+  void dispose() {
+    myController.dispose(); 
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("data"), actions: [Icon(Icons.abc_outlined)]),
-      body: Center(
-      child: Card(
-        color: Colors.red,
-       
-         child:  Text("hello card elements"),
-      
-      
-       
+      appBar: AppBar(
+        title: Text("data"),
+        actions: [Icon(Icons.abc_outlined)],
       ),
+      body: Padding(
+        padding: EdgeInsets.all(20),
+        child: Column(
+          children: [
+            TextField(
+              controller: myController,
+              decoration: InputDecoration(
+                labelText: "Enter something",
+                border: OutlineInputBorder(),
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                print(myController.text);
+              },
+              child: Text("Submit"),
+            ),
+          ],
+        ),
       ),
     );
   }
